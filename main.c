f0b6ce88e9e948ef2b497a81411c358052136672
Julia <azotowyv@gmail.com>
2019-01-12 18:01:35 +0100

    Program which emulates a bridge with maximum load 15t. The alternating traffic allows 20 cars to ride from one side to the other and then in the second way. Cars' possibly weight can be 1, 2 or 3 t.

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include <stdbool.h>
 
 
-struct sembuf semaforek;
-int sid;
+struct sembuf semaforek, kierunek;
+int sid[2];
 
 struct samochod{
     int masa;
@@ -26,13 +26,16 @@ struct samochod inicjujauto(int liczba){
 void jedz(struct samochod samochod1){
     printf("Samochod o numerze %d i wadze %d chce wjehac\n", samochod1.id, samochod1.masa);
     semaforek.sem_op=-(samochod1.masa);
-    semop(sid, &semaforek, 1);
+    semop(sid[0], &semaforek, 1);
     printf("Samochod o numerze %d i wadze %d wjezdza\n", samochod1.id, samochod1.masa);
     sleep(4);
     printf("Samochod o numerze %d i wadze %d wyjezdza\n", samochod1.id, samochod1.masa);
     semaforek.sem_op=samochod1.masa;
-    semop(sid, &semaforek,1);
+    semop(sid[0], &semaforek,1);
     printf("Samochod o numerze %d i wadze %d wyjechal\n", samochod1.id, samochod1.masa);
+    kierunek.sem_op=1;
+    semop(sid[1], &kierunek,1);
+    exit(0);
 
 }
 
@@ -40,36 +43,44 @@ void jedz(struct samochod samochod1){
 
 int main() {
     srand(time(NULL));
-    sid=semget(0x123, 1, 0600| IPC_CREAT);
-    if (sid == -1){
+    sid[0] =semget(0x123, 1, 0600| IPC_CREAT);
+    sid[1] =semget(0x124, 1, 0600| IPC_CREAT);
+    if (sid[0] == -1 || sid[1]==-1){
         perror("semafor");
         exit(1);
     }
-    semctl(sid,0,SETVAL,15);
+    semctl(sid[0],0,SETVAL,15);
     semaforek.sem_num=0;
     semaforek.sem_flg=0;
 
+    semctl(sid[1],0, SETVAL,0);
+    kierunek.sem_num=0;
+    kierunek.sem_flg=0;
 
     struct samochod probne[9];
     int i=0;
     int *stat;
 
-    printf("Z lewej na prawa\n");
-    for (i=0;i<9;i++){
-        probne[i]=inicjujauto(i);
-        if ((fork())==0){
-            jedz(probne[i]);
-        }
-    }
-    wait(stat);
-    printf("Z prawej na lewa\n");
-    for (i=0;i<9;i++){
-        probne[i]=inicjujauto(i);
-        if ((fork())==0){
-            jedz(probne[i]);
-        }
-    }
-    wait(stat);
 
+    while (1) {
+        printf("Z lewej na prawa\n");
+        for (i = 0; i < 9; i++) {
+            probne[i] = inicjujauto(i);
+            if ((fork()) == 0) {
+                jedz(probne[i]);
+            }
+        }
+        kierunek.sem_op = -9;
+        semop(sid[1], &kierunek, 1);
+        printf("Z prawej na lewa\n");
+        for (i = 0; i < 9; i++) {
+            probne[i] = inicjujauto(i + 10);
+            if ((fork()) == 0) {
+                jedz(probne[i]);
+            }
+        }
+        kierunek.sem_op = -9;
+        semop(sid[1], &kierunek, 1);
+    }
     return 0;
 }
